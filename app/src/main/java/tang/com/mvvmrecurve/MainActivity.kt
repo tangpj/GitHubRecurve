f2ec19e7d014813660927273177045c3d45aec88
TPJ <514820184@qq.com>
2018-02-27 17:46:01 +0800

    first commit

---
+++ app/src/main/java/tang/com/mvvmrecurve/MainActivity.kt
@@ -0,0 +1,12 @@
+package tang.com.mvvmrecurve
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

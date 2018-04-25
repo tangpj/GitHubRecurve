d76c584808a32b94ace908efa4e1ecfe761649f1
TPJ <514820184@qq.com>
2018-04-25 23:41:56 +0800

    add Oauth2

--- app/src/main/java/tang/com/mvvmrecurve/MainActivity.kt
+++ app/src/main/java/tang/com/mvvmrecurve/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.view.View
 import android.widget.Toast
+import tang.com.mvvmrecurve.api.GithubOauth2.Companion.launchOauthLogin
 import tang.com.mvvmrecurve.databinding.ActivityMainBinding
 import tang.com.recurve.widget.*
 
@@ -16,45 +17,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
-        binding.list.layoutManager = LinearLayoutManager(this)
-        val stringCreate = stringCreator(adapter)
-        binding.list.adapter = adapter
-        adapter.addCreator(stringCreate)
-        stringCreate.setDataList(mutableListOf("1","2","3"))
-        stringCreate.setOnItemClickListener { _, e, creatorPosition ->
-            Toast.makeText(this,"$e, creaotrPosition = " +
-                    "$creatorPosition",Toast.LENGTH_SHORT).show()
-        }
-
-        val creator = SimpleExpandableCreator<String,String>(adapter,creatorType = 2)
-        creator.setOnParentClickListener { _, parent, parentPosition, inCreatorPosition ->
-            Toast.makeText(this,"$parent, parentPosition = " +
-                    "$parentPosition, inCreatorPosition $inCreatorPosition",Toast.LENGTH_SHORT).show()
-        }
-        creator.setOnChildClickListener { _, child, childPosition, inCreatorPosition ->
-            Toast.makeText(this,"$child, childPosition = " +
-                    "$childPosition, inCreatorPosition $inCreatorPosition",Toast.LENGTH_SHORT).show()
-        }
-        adapter.addCreator(creator)
-        val linked = LinkedHashMap<String,MutableList<String>>()
-
-        linked["2222"] = mutableListOf("4","5","6")
-        linked["3333"] = mutableListOf("7","8")
-        creator.setDataList(linked)
-        val stringCreate1 = stringCreator(adapter,1)
-        stringCreate1.setOnItemClickListener { _, e, creatorPosition ->
-            Toast.makeText(this,"$e, creaotrPosition = " +
-                    "$creatorPosition",Toast.LENGTH_SHORT).show()
-        }
-        stringCreate1.setDataList(mutableListOf("99999","8888888","666666"))
-        adapter.addCreator(stringCreate1)
 
 
     }
 
     fun add(v: View){
-        val stringCreate1 = stringCreator(adapter,1)
-        stringCreate1.setDataList(mutableListOf("4","5","6"))
-        adapter.addCreator(stringCreate1)
+        launchOauthLogin(this)
     }
 }
--- app/src/main/java/tang/com/mvvmrecurve/api/GithubOauth2.kt
+++ app/src/main/java/tang/com/mvvmrecurve/api/GithubOauth2.kt
@@ -1,12 +1,36 @@
 package tang.com.mvvmrecurve.api
 
-class GithubOauth2{
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import tang.com.mvvmrecurve.BuildConfig
+import tang.com.mvvmrecurve.R
+import tang.com.recurve.util.IntentUtils
+import tang.com.recurve.util.UiUtils
+import javax.inject.Inject
+
+class GithubOauth2 private constructor(){
 
     companion object {
         const val OAUTH_URL = "https://github.com/login/oauth/authorize"
-        private val PARAM_CLIENT_ID = "client_id"
-        private val PARAM_CODE = "code"
-        private val PARAM_SCOPE = "scope"
-        private val PARAM_CALLBACK_URI = "redirect_uri"
+        const val PARAM_CLIENT_ID = "client_id"
+        const val PARAM_CODE = "code"
+        const val PARAM_SCOPE = "scope"
+        const val PARAM_CALLBACK_URI = "redirect_uri"
+        const val SCOPES = "user,repo,gist"
+
+        fun launchOauthLogin(activity: Activity) {
+            val uri = Uri.parse(OAUTH_URL)
+                    .buildUpon()
+                    .appendQueryParameter(PARAM_CLIENT_ID, BuildConfig.CLIENT_ID)
+                    .appendQueryParameter(PARAM_SCOPE, SCOPES)
+                    .appendQueryParameter(PARAM_CALLBACK_URI, BuildConfig.REDIRECTt_URI)
+                    .build()
+            IntentUtils.openInCustomTabOrBrowser(activity,uri)
+        }
+
     }
+
+
+
 }

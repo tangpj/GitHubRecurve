b12cd8f73a20b25754241fa0d2775d1f90f2a567
TPJ <514820184@qq.com>
2018-04-28 15:58:21 +0800

    update gradle

--- github_base/src/main/java/tang/com/oauth2/GithubOauth2.kt
+++ github_base/src/main/java/tang/com/oauth2/GithubOauth2.kt
@@ -1,13 +1,9 @@
-package tang.com.mvvmrecurve.api
+package tang.com.oauth2
 
 import android.app.Activity
-import android.content.Intent
 import android.net.Uri
-import tang.com.mvvmrecurve.BuildConfig
-import tang.com.mvvmrecurve.R
+
 import tang.com.recurve.util.IntentUtils
-import tang.com.recurve.util.UiUtils
-import javax.inject.Inject
 
 class GithubOauth2 private constructor(){
 
@@ -17,13 +13,13 @@ class GithubOauth2 private constructor(){
         const val PARAM_CODE = "code"
         const val PARAM_SCOPE = "scope"
         const val PARAM_CALLBACK_URI = "redirect_uri"
-        const val SCOPES = "user,repo,gist"
 
+        @JvmStatic
         fun launchOauthLogin(activity: Activity) {
             val uri = Uri.parse(OAUTH_URL)
                     .buildUpon()
                     .appendQueryParameter(PARAM_CLIENT_ID, BuildConfig.CLIENT_ID)
-                    .appendQueryParameter(PARAM_SCOPE, SCOPES)
+                    .appendQueryParameter(PARAM_SCOPE, BuildConfig.SCOPES)
                     .appendQueryParameter(PARAM_CALLBACK_URI, BuildConfig.REDIRECTt_URI)
                     .build()
             IntentUtils.openInCustomTabOrBrowser(activity,uri)

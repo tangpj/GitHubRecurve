93136387b1922acbd2474bd41f458741a7192e39
TPJ <514820184@qq.com>
2018-11-26 23:16:30 +0800

    add fragment_profile

--- baselib/github_base/src/main/java/com/tangpj/github/BaseActivity.kt
+++ baselib/github_base/src/main/java/com/tangpj/github/BaseActivity.kt
@@ -1,11 +1,9 @@
 package com.tangpj.github
 
-import android.content.SharedPreferences
 import android.os.Bundle
 import com.tangpj.github.di.THEME_ID
 import com.tangpj.github.di.installAppThemeSp
 import dagger.android.support.DaggerAppCompatActivity
-import javax.inject.Inject
 
 abstract class BaseActivity : DaggerAppCompatActivity(){
 

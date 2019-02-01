64f3e417cecac19e6b7b6e1a183fb087e5ab5bbd
TPJ <514820184@qq.com>
2019-02-01 14:52:25 +0800

    support RecurveActivity navigation

--- modules/oauth2/src/main/java/com/tangpj/oauth2/ui/AuthorizationActivity.kt
+++ modules/oauth2/src/main/java/com/tangpj/oauth2/ui/AuthorizationActivity.kt
@@ -2,25 +2,19 @@ package com.tangpj.oauth2.ui
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import com.tangpj.github.BaseActivity
 import com.tangpj.oauth2.GithubOauth2.Companion.PARAM_CODE
 import com.tangpj.oauth2.R
-import com.tangpj.oauth2.databinding.ActivityOauth2Binding
 
 
 class AuthorizationActivity: BaseActivity() {
-
-
-    lateinit var binding: ActivityOauth2Binding
 
     lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        navController = initContentFragment<ActivityOauth2Binding>(R.navigation.navigation_oauth2)
+        navController = initContentFragment(R.navigation.navigation_oauth2)
         intent?.data?.getQueryParameter(PARAM_CODE)?.let {
             navController.navigate(AuthorizationFragmentDirections.refreshCode().setCode(it))
         }
@@ -31,7 +25,7 @@ class AuthorizationActivity: BaseActivity() {
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
         intent?.data?.getQueryParameter(PARAM_CODE)?.let {
-            navController.navigate(AuthorizationFragmentDirections.refreshCode().setCode(it))
+            navController?.navigate(AuthorizationFragmentDirections.refreshCode().setCode(it))
         }
     }
 
--- modules/repository/src/main/java/com/tangpj/repository/ui/repositories/RepositoriesActivity.kt
+++ modules/repository/src/main/java/com/tangpj/repository/ui/repositories/RepositoriesActivity.kt
@@ -1,22 +1,17 @@
 package com.tangpj.repository.ui.repositories
 
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import com.tangpj.github.BaseActivity
 import com.tangpj.repository.R
-import com.tangpj.repository.databinding.ActivityRepositoriesBinding
 
 class RepositoriesActivity : BaseActivity(){
 
     lateinit var navController: NavController
 
-    lateinit var activityRepositoriesBinding: ActivityRepositoriesBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        navController = initContentFragment<ActivityRepositoriesBinding>(
+        navController = initContentFragment(
                 R.navigation.navigation_repositories)
         appbar{
             title = getString(R.string.app_name)

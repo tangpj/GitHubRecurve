cd023d86be413829dadc3db6059ad5d4df200e68
TPJ <514820184@qq.com>
2019-02-04 23:26:38 +0800

    update RepoRepository

--- modules/repository/src/main/java/com/tangpj/repository/repository/RepoRepository.kt
+++ modules/repository/src/main/java/com/tangpj/repository/repository/RepoRepository.kt
@@ -1,4 +1,19 @@
 package com.tangpj.repository.repository
 
-class RepoRepository{
+import androidx.lifecycle.LiveData
+import com.apollographql.apollo.ApolloClient
+import com.tangpj.github.vo.Repo
+import com.tangpj.recurve.resource.Resource
+import com.tangpj.recurve.util.RateLimiter
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
+
+class RepoRepository @Inject constructor(@Inject val apolloClient: ApolloClient){
+
+    private val repoRateLimiter = RateLimiter<Repo>(10, TimeUnit.MINUTES)
+
+    fun loadRepos(owner: String): LiveData<Resource<List<Repo>>>{
+
+    }
+
 }

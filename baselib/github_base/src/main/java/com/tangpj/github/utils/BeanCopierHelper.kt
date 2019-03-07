017151f95188591452315ea54cb95469d82bb05c
TPJ <514820184@qq.com>
2019-03-07 20:17:25 +0800

    cglib not supper android，remove it

--- baselib/github_base/src/main/java/com/tangpj/github/utils/BeanCopierHelper.kt
+++ baselib/github_base/src/main/java/com/tangpj/github/utils/BeanCopierHelper.kt
@@ -1,8 +1,5 @@
 package com.tangpj.github.utils
 
-import net.sf.cglib.beans.BeanCopier
-import net.sf.cglib.core.Converter
-import java.util.concurrent.ConcurrentHashMap
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -16,25 +13,7 @@ import javax.inject.Singleton
 @Singleton
 class BeanCopierHelper @Inject constructor(){
 
-    private val copierMap: Map<String, BeanCopier> = ConcurrentHashMap()
 
-    @JvmOverloads
-    fun <T: Any> copy(from: Any, to: T, converter: Converter? = null): T{
-        val useConverter = converter != null
-        val key = from.javaClass.name + to.javaClass.name
-        val copier: BeanCopier = if (copierMap.containsKey(key)){
-            copierMap[key] ?: BeanCopier.create(from.javaClass, to.javaClass, useConverter)
-        }else{
-            BeanCopier.create(from.javaClass, to.javaClass, useConverter)
-        }
-
-        if (useConverter){
-            copier.copy(from, to, null)
-        }else{
-            copier.copy(from, to, converter)
-        }
-        return to
-    }
 
 }
 

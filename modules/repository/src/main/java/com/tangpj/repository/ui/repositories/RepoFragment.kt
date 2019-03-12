b67365e1fa75177c0cee306c1d33c7b4ce6b7428
TPJ <514820184@qq.com>
2019-03-12 23:47:39 +0800

    RepoFragment add loading layout

--- modules/repository/src/main/java/com/tangpj/repository/ui/repositories/RepoFragment.kt
+++ modules/repository/src/main/java/com/tangpj/repository/ui/repositories/RepoFragment.kt
@@ -2,9 +2,13 @@ package com.tangpj.repository.ui.repositories
 
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.RecyclerView
+import com.tangpj.github.databinding.FragmentBaseRecyclerViewBinding
 import com.tangpj.recurve.dagger2.RecurveDaggerListFragment
 import com.tangpj.repository.creator.RepositoryCreator
 import timber.log.Timber
@@ -24,20 +28,30 @@ class RepoFragment: RecurveDaggerListFragment() {
         super.onActivityCreated(savedInstanceState)
         val arg = RepoFragmentArgs.fromBundle(arguments)
         Timber.d("user name = ${arg.userName}")
-        repoViewModel = ViewModelProviders.of(this, viewModelFactory)
-                .get(RepositoryViewModel::class.java)
-        repoViewModel.starRepo.observeForever {
-            it.data?.let { items ->
-                repositoryCreator.addItems(items)
+
+        repoViewModel.repos.observeForever { repos ->
+            repos?.let {
+                repositoryCreator.addItems(it)
             }
         }
         repoViewModel.setRepoOwner("Tangpj")
     }
 
+    override fun onCreateBinding(
+            inflater: LayoutInflater,
+            container: ViewGroup?, savedInstanceState: Bundle?): ViewDataBinding {
+        repoViewModel = ViewModelProviders.of(this, viewModelFactory)
+                .get(RepositoryViewModel::class.java)
+        val binding = FragmentBaseRecyclerViewBinding.inflate(inflater, container, false)
+        initRecyclerView(binding.recyclerContent.rv)
+        binding.loadingContent.resource = repoViewModel.resource.value
+        binding.setLifecycleOwner(this)
+        return binding
+    }
+
     override fun initRecyclerView(rv: RecyclerView) {
         super.initRecyclerView(rv)
         addItemCreator(repositoryCreator)
-
 
     }
 
--- modules/repository/src/main/java/com/tangpj/repository/ui/repositories/RepositoryViewModel.kt
+++ modules/repository/src/main/java/com/tangpj/repository/ui/repositories/RepositoryViewModel.kt
@@ -1,18 +1,29 @@
 package com.tangpj.repository.ui.repositories
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import com.tangpj.recurve.resource.Resource
 import com.tangpj.repository.repository.RepoRepository
+import com.tangpj.repository.vo.RepoVo
 import javax.inject.Inject
 
 class RepositoryViewModel @Inject constructor(private val repoRepository: RepoRepository): ViewModel(){
 
-    private val _login: MutableLiveData<String> = MutableLiveData()
+    private val _login = MutableLiveData<String>()
 
-    val starRepo = Transformations.switchMap(_login){
+
+    private val starRepo: LiveData<Resource<List<RepoVo>>> = Transformations.switchMap(_login){
         repoRepository.loadRepos(it)
     }
+
+    val repos: LiveData<List<RepoVo>> = Transformations.map(starRepo){
+        resource.value = it
+        it.data
+    }
+
+    val resource: MutableLiveData<Resource<*>> = MutableLiveData()
 
     fun setRepoOwner(login: String){
         _login.value = login

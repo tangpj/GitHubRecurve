4baccca2bc188f98634ba650d497d42ad974cb36
TPJ <514820184@qq.com>
2019-04-02 18:03:19 +0800

    update repo ui

--- modules/repository/src/main/java/com/tangpj/repository/mapper/RepoMapper.kt
+++ modules/repository/src/main/java/com/tangpj/repository/mapper/RepoMapper.kt
@@ -15,6 +15,8 @@ interface RepoMapper{
             Mapping(source = "repoDto.name", target = "name"),
             Mapping(source = "repoDto.stargazers.totalCount", target = "stars"),
             Mapping(source = "repoDto.forks.totalCount", target = "forks"),
+            Mapping(target = "fullName",
+                    expression = "java(repoDto.getOwner().getLogin() + \"/\" + repoDto.getName())"),
             Mapping(source = "languageDto.name", target = "language"),
             Mapping(source = "languageDto.color", target = "languageColor"))
     fun from(repoDto: RepoDto, languageDto: LanguageDto?): RepoVo

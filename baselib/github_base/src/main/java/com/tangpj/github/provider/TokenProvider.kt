dfa4b967612a9f35270c3bbca208b7fa8019ea24
TPJ <514820184@qq.com>
2019-04-02 22:13:19 +0800

    add TokenProvider

---
+++ baselib/github_base/src/main/java/com/tangpj/github/provider/TokenProvider.kt
@@ -0,0 +1,40 @@
+package com.tangpj.github.provider
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.database.Cursor
+import android.net.Uri
+
+/**
+ * 组建模式下提供Token数据用于其它组建的登陆
+ *
+ * @ClassName: TokenProvider
+ * @author create by Tang
+ * @date 2019-04-02 21:55
+ */
+class TokenProvider : ContentProvider(){
+    override fun insert(uri: Uri, values: ContentValues?): Uri? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun query(uri: Uri, projection: Array<String>?, selection: String?, selectionArgs: Array<String>?, sortOrder: String?): Cursor? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreate(): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun update(uri: Uri, values: ContentValues?, selection: String?, selectionArgs: Array<String>?): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getType(uri: Uri): String? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}

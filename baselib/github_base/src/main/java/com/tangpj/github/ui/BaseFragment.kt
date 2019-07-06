685e8ae564bcb5db1ec70302f923199faa7a8157
TPJ <514820184@qq.com>
2019-07-07 00:17:52 +0800

    fix Loading bug

--- baselib/github_base/src/main/java/com/tangpj/github/ui/BaseFragment.kt
+++ baselib/github_base/src/main/java/com/tangpj/github/ui/BaseFragment.kt
@@ -2,6 +2,7 @@ package com.tangpj.github.ui
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import androidx.databinding.ViewDataBinding
@@ -10,12 +11,14 @@ import com.tangpj.github.databinding.FragmentBaseBinding
 import com.tangpj.github.ui.creator.ItemLoadingCreator
 import com.tangpj.paging.PageLoadStatus
 import com.tangpj.recurve.dagger2.RecurveDaggerFragment
+import com.tangpj.recurve.resource.Status
 import com.tangpj.recurve.ui.fragment.RecurveFragment
 import com.tangpj.recurve.ui.strategy.LoadingStrategy
 
 abstract class BaseFragment : RecurveDaggerFragment(){
 
     private lateinit var _binding: FragmentBaseBinding
+    private lateinit var contentBinding: ViewDataBinding
 
     abstract fun onCreateContentBinding(inflater: LayoutInflater, container: ViewGroup?) : ViewDataBinding
 
@@ -25,7 +28,7 @@ abstract class BaseFragment : RecurveDaggerFragment(){
 
     final override fun onCreateBinding(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): ViewDataBinding? {
         _binding = FragmentBaseBinding.inflate(inflater, container, false)
-        val contentBinding = onCreateContentBinding(inflater, _binding.root as? ViewGroup)
+        contentBinding = onCreateContentBinding(inflater, _binding.root as? ViewGroup)
         _binding.setLifecycleOwner(this)
         contentBinding.setLifecycleOwner(this)
         _binding.flRoot.addView(contentBinding.root)
@@ -34,11 +37,17 @@ abstract class BaseFragment : RecurveDaggerFragment(){
         return _binding
     }
 
-    fun <Data> loading(loadingInvoke : Loading<Data>.() -> Unit){
+    fun <Data > loading(loadingInvoke : Loading<Data>.() -> Unit){
         val loading = Loading<Data>()
         loading.loadingInvoke()
-        loading.resource = loading.resource
-        loading.retry = loading.retry
+        loading.resource?.observe(this, Observer {
+            contentBinding.root.visibility = if (it.networkState.status == Status.SUCCESS){
+                View.VISIBLE
+            }else{
+                View.GONE
+            }
+            _binding.resource = it
+        })
 
     }
 }
